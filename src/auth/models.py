1bb1343e007542a962f94295f9ba589db176dcfe
anton <contact@sorridi.xyz>
2024-10-08 18:35:14 +0200

    more realistic limits for input fields

--- src/auth/models.py
+++ src/auth/models.py
@@ -11,12 +11,12 @@ app_name = "auth"
 @register(database, app_name)
 class User(models.Model):
     uuid = models.UUIDField(primary_key=True)
-    username = models.CharField(max_length=50, unique=True)
+    username = models.CharField(max_length=16, unique=True)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
+    password = models.CharField(max_length=32)
     image = models.ImageField()
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
+    name = models.CharField(max_length=32)
+    surname = models.CharField(max_length=32)
 
     last_login = models.DateTimeField()
     date_joined = models.DateTimeField()
