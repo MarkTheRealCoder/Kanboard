2e7529d4e83568f5e3bf88d93ecb79fe743ea645
MRC_CODE <marcolacivita400@gmail.com>
2024-10-08 18:22:37 +0200

    Views and validations.
    Added display views for static pages (no_auth).
    Added working views for user management and logout.
    Added display view for dashboard.
    Initialized working view for board creation.

--- src/auth/models.py
+++ src/auth/models.py
@@ -11,13 +11,13 @@ app_name = "auth"
 @register(database, app_name)
 class User(models.Model):
     uuid = models.UUIDField(primary_key=True)
-    username = models.CharField(max_length=100)
-    email = models.EmailField()
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
-    image = models.ImageField(width_field=100, height_field=100)
-
+    image = models.ImageField()
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
+
     last_login = models.DateTimeField()
     date_joined = models.DateTimeField()
 
